1b4e86d4428cf6d1f82257a7722325745e0f12ef
Jon <ntjeddelog@gmail.com>
2026-02-06 10:49:10 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+void main() {
+        Character hero = new Character(100, 50);
+        hero.swingSword();
+        hero.swingSword();
+        hero.swingSword();
+        hero.swingSword();
+        hero.swingSword();
+        hero.takeDamage(30);
+        hero.takeDamage(50);
+        hero.takeDamage(20);
+        hero.rest();
+        System.out.println("Character's health: " + hero.getHealth() + ", stamina: " + hero.getStamina() + ".");
+    }
+}
